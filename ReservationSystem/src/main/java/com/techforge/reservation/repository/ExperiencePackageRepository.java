122cd233a35c948b7e957d53fa778edba5819b4b
Inbanithi <inbanithi107@gmail.com>
2026-06-24 20:13:40 +0530

    added repositories

---
+++ ReservationSystem/src/main/java/com/techforge/reservation/repository/ExperiencePackageRepository.java
@@ -0,0 +1,7 @@
+package com.techforge.reservation.repository;
+
+import com.techforge.reservation.entity.ExperiencePackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperiencePackageRepository extends JpaRepository<ExperiencePackage, Long> {
+}
